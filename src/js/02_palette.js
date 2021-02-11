b41e35b706c95c4f85d8e159cca4c97f5dc3c7b8
NataliaMigallon <natmigallon@gmail.com>
2021-02-11 17:53:34 +0100

    Add typo inputs

--- src/js/02_palette.js
+++ src/js/02_palette.js
@@ -2,6 +2,7 @@
 
 const paletteElements = document.querySelectorAll(".js-palette");
 const cardElement = document.querySelector(".js-card");
+const typoElements = document.querySelector(".js-typo");
 
 function updatePalette() {
   cardElement.classList.remove("palette-1", "palette-2", "palette-3");
@@ -15,6 +16,17 @@ for (const paletteElement of paletteElements) {
   paletteElement.addEventListener("change", updatePalette);
 }
 
+function updateTypo() {
+  cardElement.classList.remove("typo-1", "typo-2", "typo-3");
+  const checkedTypo = document.querySelector(".js-typo:checked");
+  const checkedTypoValue = checkedTypo.value;
+  cardElement.classList.add("typo-" + checkedTypoValue);
+  saveInLocalStorage();
+}
+
+for (const typoElement of typoElements) {
+  typoElement.addEventListener("change", updateTypo);
+}
 // ESTO DE ABAJO ES LO QUE TENÍAMOS ANTERIORMENTE
 /* handlePalette(); */
 
