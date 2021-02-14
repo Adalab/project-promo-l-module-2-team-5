d3c9f99f9f5eee0879975288a57abd65189d7954
Marina Delgado <marinadelges@gmail.com>
2021-02-14 19:56:56 +0100

    fix

--- src/js/07_twitter.js
+++ src/js/07_twitter.js
@@ -2,6 +2,7 @@ const twitterElement = document.querySelector('.js-twitter-btn');
 const twitterLinkElement = document.querySelector('.js-twitter-link');
 
 function handleTwitterShare(data) {
+
     twitterLinkElement.href = `https://twitter.com/intent/tweet?text=Aquí%20tienes%20mi%20nueva%20tarjeta%20${data.cardURL}`;
 }
 twitterElement.addEventListener('click', handleTwitterShare);
