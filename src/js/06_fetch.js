22571c20c3eea7cc64f64dbbe800458efeff871e
marina-fp <marina.fdezpas@gmail.com>
2021-02-14 22:33:04 +0100

    Add grey create button

--- src/js/06_fetch.js
+++ src/js/06_fetch.js
@@ -4,13 +4,13 @@ const createBtn = document.querySelector(".js-create-btn");
 const cardResultElement = document.querySelector(".js-card-result");
 const urlShareElement = document.querySelector(".js-share__link");
 const shareContainer = document.querySelector(".js-share__container");
+const createBtnContainer = document.querySelector(".js-createbtn-container");
 
 function handleCreateBtn(ev) {
   ev.preventDefault();
   console.log("Mis datos", getUserData());
 
-  const url =
-    "https://profileawesome.herokuapp.com/card";
+  const url = "https://profileawesome.herokuapp.com/card";
   const data = getUserData();
 
   fetch(url, {
@@ -25,8 +25,11 @@ function handleCreateBtn(ev) {
       console.log("Server response:", data);
       if (data.success === true) {
         cardResultElement.innerHTML = data.cardURL;
-        urlShareElement.innerHTML = data.cardURL; //para que pinte en la interfaz
+        urlShareElement.innerHTML = data.cardURL;
         shareContainer.classList.remove("hidden");
+        createBtnContainer.classList.remove("collapsable__share");
+        createBtnContainer.classList.add("collapsable__share--clicked")
+        createBtn.disable = true;
       } else {
         cardResultElement.innerHTML = data.error;
         urlShareElement.innerHTML = "ERROR. No se ha podido crear la tarjeta";
